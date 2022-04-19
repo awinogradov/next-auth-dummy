921e0a7bfca5777af3425cdcdfd62f8f92d6fe23
Anton Vinogradov <AAlVinogradov@sberbank.ru>
2022-04-19 10:20:24 +0300

    init

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,13 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import { useSession } from "next-auth/react"
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+  const { data: session, status } = useSession()
+
+  console.log(session, status);
+
   return (
     <div className={styles.container}>
       <Head>
